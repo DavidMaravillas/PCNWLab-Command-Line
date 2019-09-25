77c8679a3bc7528892c60c703b00b3af8e9c9299
david maravillas <davidmaravillas@pursuit.org>
2019-09-25 05:45:54 -0400

    test01

--- my_first_direc/pset1.js
+++ my_first_direc/pset1.js
@@ -6,18 +6,28 @@ Follow the following steps ONLY using your command line:
 1. Navigate to your Desktop directory.
 cd Desktop
 2. Create a directory called my_first_direc.
-
+mkdir my_first_direc
 3. Enter the my_first_direc directory.
+cd my_first direc
 4. Create a file called first.js.
+touch first.js
 5. Create a folder called assignments.
+cd ..
+mkdir assignments
 6. Move first.js into assignments.
+mv first.js ../assignments
 7. Go into the assignments folder.
+cd ../assginments
 8. Delete first.js.
+rm first.js
 9. Go up to the my_first_direc directory.
+cd ../my_first_direc
 10. Delete the assignments folder.
+rmdir assignments
 11. Go up to the Desktop directory.
+cd ..
 12. Delete the test folder.
-
+rm -r my_first_direc
 ******************/
 
 
@@ -28,10 +38,11 @@ PROBLEM 2:
 Follow the following steps ONLY using your command line:
 
 1. Navigate to the inside of your Documents folder
+cd ~/Documents
 3. Create a file called foo.js
 touch foo.js
 4. Open the file with a text editor, (Atom/VSCode)
-VScode foo.js
+code . 
 5. Add the following text to the file: console.log('hello')
 6. Save and exit the file
 7. Open the file again with a text editor, (Atom/VSCode)
